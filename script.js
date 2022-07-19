1f1bf0b42a636c59d1655c35b8e3a16516a3c11f
ChrisDelvalle90 <christian_mskt@hotmail.com>
2022-07-19 15:55:56 -0400

    Criacao da pagina que exibe a situação do aluno

---
+++ script.js
@@ -0,0 +1,73 @@
+let listaNotas = []
+let soma = 0
+let media = 0
+
+function adicionar()
+{
+    soma = 0;
+    let nota = document.getElementById("nota").value
+    if(nota == "")
+    {
+        alert("Preencha o campo!")
+    }
+    else
+    {
+        listaNotas.push(nota)
+        for(let i = 0; i < listaNotas.length; i++)
+        {
+            soma += parseFloat(listaNotas[i])
+        }
+        document.getElementById("nota").value = ""
+        document.getElementById("tNotas").innerText = listaNotas.join(" , ")
+        document.getElementById("addNotas").innerText = listaNotas.join(" , ")
+        maiorNota()
+        menorNota()
+        calcMedia()
+        situacaoAluno()
+    }
+}
+
+function substituiNota()
+{
+    
+}
+
+function maiorNota()
+{
+    maior = Math.max(...listaNotas)
+    document.getElementById("maiorNota").innerText = maior
+}
+
+function menorNota()
+{
+    menor = Math.min(...listaNotas)
+    document.getElementById("menorNota").innerText = menor
+}
+
+function calcMedia()
+{
+    media =  (soma / listaNotas.length)
+    document.getElementById("media").innerText = media.toFixed(1)
+}
+
+function situacaoAluno()
+{
+    let situacao = document.getElementById("situacao")
+
+    if(media < 4)
+    {
+        situacao.innerText = "ALUNO REPROVADO"
+        situacao.style.color = "red"
+    }
+    else if(media >= 4 && media < 7)
+    {
+        situacao.innerText = "RECUPERAÇÃO"
+        situacao.style.color = "yellow"
+    }
+    else
+    {
+        situacao.innerText = "APROVADO"
+        situacao.style.color = "green"
+    }
+
+}
